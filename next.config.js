1c1bdae5e9273c27c0fede0f9c719b5c225e299a
sv456rty <sv456rty@gmail.com>
2023-09-04 21:02:03 -0700

    Updated packages.json & next.config.js

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const withNextIntl = require("next-intl/plugin")(
-  // This is the default (also the `src` folder is supported out of the box)
-  "./src/i18n.ts"
-);
-
 const nextConfig = {
   experimental: { optimizeCss: true },
   images: {
@@ -20,8 +15,6 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-module.exports = withBundleAnalyzer(
-  withNextIntl({
-    ...nextConfig,
-  })
-);
+module.exports = withBundleAnalyzer({
+  ...nextConfig,
+});
